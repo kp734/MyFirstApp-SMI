3f6d92053b427b32cf468a130c652e8f4827b27d
Kevin Patel <60721358+kp734@users.noreply.github.com>
2021-05-19 11:23:01 -0400

    Added Textfield

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var labelResult: UILabel!
+    @IBOutlet var textMessage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeButton(_ sender: UIButton) {
+        labelResult.text = textMessage.text?.uppercased()
+    }
 }
 
